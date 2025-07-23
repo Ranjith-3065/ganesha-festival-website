bdf4d31c02f7c4bba1793cae2e80ea5ad9368f33
Ranjith <ranjith20061917@gmail.com>
2025-07-23 22:52:43 +0530

    🎨 Update

--- backend/routes/gallery.js
+++ backend/routes/gallery.js
@@ -76,15 +76,6 @@ router.delete("/:filename", async (req, res) => {
 });
 
 
-// moments wall
-router.get("/getAllImages", async (req, res) => {
-  try {
-    const images = await Image.find().sort({ createdAt: -1 });
-    res.json(images);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to fetch images" });
-  }
-});
 
 
 module.exports = router;
