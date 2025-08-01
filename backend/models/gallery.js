20a8f08a16b4d36e0da16292518ad2052cea65ea
Ranjith <ranjith20061917@gmail.com>
2025-08-01 16:43:54 +0530

    🎨 Update

--- backend/models/gallery.js
+++ backend/models/gallery.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const GallerySchema = new mongoose.Schema({
-  filename: String,
-  caption: String,
+  filename: { type: String, required: true },
+  publicId: { type: String, required: true },   // Cloudinary public_id
+  caption: { type: String, default: "Untitled" },
+  url: { type: String, required: true },        // Cloudinary image URL
   uploadedAt: {
     type: Date,
     default: Date.now
